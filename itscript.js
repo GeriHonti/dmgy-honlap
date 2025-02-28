88403c85b8aada6cfbf4101c95810af102b3d2ce
Dr. Gergely Marcell Honti <honti@honti.us>
2025-02-28 20:47:57 +0100

    Rohn Erzsebet preach.

--- itscript.js
+++ itscript.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("data.json")
+    fetch("data/data.json")
         .then(response => response.json())
         .then(data => {
             const today = new Date();
